525f888147ecddd39d21c8ca08b626190032223e
Echaarani Mohamed <echaaranimohamed@gmail.com>
2019-10-09 15:34:13 +0100

    adding cors package

--- src/app.js
+++ src/app.js
@@ -2,10 +2,12 @@ const express = require('express');
 const app = express();
 const userRouter = require('./Routers/users-router')
 const taskRouter = require('./Routers/tasks-router')
+const cors = require('cors')
 require('./db/mongoose')
 
 app.use(express.json())
 app.use(userRouter)
 app.use(taskRouter)
+app.use(cors())
 
 module.exports = app
